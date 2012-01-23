cbe7f124cfcd249ac0a123f7bac31356ba7f990e
Matthew Laver <matthewlaver@gmail.com>
2012-01-23 16:20:31 +0000

    made results view show on reset

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ App = Backbone.Router.extend({
 		"*page" : "start"
 	},
 	start: function(){
-		console.log('start');
 		var starttmplt = _.template($('#start').html());
 		$('#contents').html(starttmplt);
 	},
 	startSearch: function(){
-		console.log('start search function');
 		searchView = new SearchView;
 	}
 });
--- shots.js
+++ shots.js
@@ -1,8 +1,16 @@
+//create the model, but details will be added from the search results
 Shot = Backbone.Model.extend({});
 
-window.ShotsList = Backbone.Collection.extend({
-	
+//manage the collection of shot models and render results on reset
+ShotList = Backbone.Collection.extend({
 	model: Shot,
+	el: '#results',
+	initialize: function() {
+		_.bindAll(this, 'addOne', 'render');
+		this.bind('add', this.addOne);
+		this.bind('reset', this.render);
+	},
+	//this limits results when collection is fetched
 	parse: function(response){
 		if(response.query.count > 0){
 			return response.query.results.a;	
@@ -12,6 +20,7 @@ window.ShotsList = Backbone.Collection.extend({
 		}
 		
 	},
+	//fetch shots from dribble. 'search' is set in the view.
 	url: function() {
 		var searchRoot = "http://query.yahooapis.com/v1/public/yql?q=";
 		var searchUrl = "select * from html where url='http://dribbble.com/search?page=1&q=";
@@ -22,9 +31,19 @@ window.ShotsList = Backbone.Collection.extend({
 
 		return searchRoot + encodeURIComponent(searchUrl) + searchEnd;
 		//return "http://api.dribbble.com/players/" + encodeURIComponent(this.search) +"/shots?per_page=18";
-	}
-
-});
+	},
+	addOne: function(shot) {
+		//pass the individual model to a new shotvie then render
+		var view = new ShotView({model: shot});
+		$(this.el).append(view.render().el);
+		
+	},
+	render: function(collection){
+		$(this.el).html('<div style="clear:both;"></div>');
+		this.each(this.addOne);
+		//Shots.render();
+	},
+}); 
 
 window.ShotView = Backbone.View.extend({
 	
@@ -34,19 +53,19 @@ window.ShotView = Backbone.View.extend({
 	events: {
 		"click a" : "clicked"
 	},
-
 	render: function(){
-		var img = this.model.get("img");
-		$(this.el).html(this.template({
+		var img = this.model.get("img"),
+		html = this.template({
 			id: this.id,
 			url: img['src'],
 			link: this.model.get("href")
-		}));
+		});
+
+		$(this.el).append(html);
 		return this;
 	},
 	clicked: function(e) {
 		e.preventDefault();
-		var id = $(e.currentTarget).data("id");
 		console.log(e.currentTarget);
 	}
 
