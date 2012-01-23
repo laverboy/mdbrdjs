443981b367aa524bc18ce761f263c90048e93e95
Matthew Laver <matthewlaver@gmail.com>
2012-01-23 16:20:31 +0000

    made results view show on reset

--- searchview.js
+++ searchview.js
@@ -6,13 +6,7 @@ window.SearchView = Backbone.View.extend({
 		"keypress #entrybox": "searchOnEnter"
 	},
 	initialize: function() {
-		console.log('SearchView intialised');
-		window.Shots = new ShotsList;
-
-		_.bindAll(this, 'addOne', 'addAll');
-		
-		Shots.bind('add', this.addOne);
-		Shots.bind('reset', this.addAll);
+		window.Shots = new ShotList;
 		this.render();
 	},
 	render: function(){
@@ -20,19 +14,10 @@ window.SearchView = Backbone.View.extend({
 	},
 	searchOnEnter: function(e) {
 		if(e.keyCode !=13) return;
-		Shots.search = encodeURIComponent($('#entrybox').val());
 		$('#search').addClass('loading');
-		Shots.fetch({success: this.searchSuccess, error: this.searchError});
 
-	},
-	addOne: function(shot) {
-		var view = new ShotView({model: shot});
-		this.$('#results').append(view.render().el);
-	},
-	addAll: function(collection){
-		console.log('addAll');
-		$('#results').html('<div style="clear:both;"></div>');
-		Shots.each(this.addOne);
+		Shots.search = encodeURIComponent($('#entrybox').val());
+		Shots.fetch({success: this.searchSuccess, error: this.searchError});
 	},
 	searchSuccess: function(){
 		$('#search').removeClass('loading');
