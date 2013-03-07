f8d915be389b8946202149797bec94a31dc32bde
Matthew Laver <matthewlaver@gmail.com>
2013-03-07 17:24:05 -0500

    connected to mongolab

--- app.js
+++ app.js
@@ -1,34 +1,69 @@
-var app = angular.module('mdbrd', ['ngResource'], function ($provide) {
-    $provide.factory('mdbrddb', ['$http', function(http) {
-        var items = [];
-        var modify = {
-            addItem: function  (item) {
-                item.bigImageUrl = this.getBigImage(item);
-                items.push(item);
-            },
-            removeItem: function (item) { items.splice(items.indexOf(item), 1); },
-            toggleItem: function (item) {
-                ( this.hasItem(item) ) ? this.removeItem(item) : this.addItem(item);
-            },
-            getItems: function () { return items; },
-            hasItem: function (item) {
-                return ( _.find(items, function (i) { return i.id === item.id; }) ) ? true : false;
-            },
-            getBigImage: function (item) {
-                http.jsonp('http://api.dribbble.com/shots/' + item.id + '?callback=JSON_CALLBACK')
-                .success(function (response) {
-                    item.bigImageUrl = response.image_url;
-                });
-            }
-            
-        };
-        return modify;
-    }]);
+/*global angular, _ */
+
+var app = angular.module('mdbrd', ['ngResource']);
+
+app.factory('Mongodb', function ($resource) {
+    var mongolab = $resource('https://api.mongolab.com/api/1/databases' +
+        '/moodboards/collections/mdbrds/:id',
+        { apiKey: 'dj1dHpCLgc0tT_9wzAE2d2670XleOZsX' }, {
+            update: { method: 'PUT' }
+        }
+    );
+    
+    return mongolab;
 });
 
-app.run( function($rootScope) {
+app.factory('mdbrddb', ['$http', 'Mongodb', function(http, Mongodb) {
+    var items = [];
+    var mdbrddb = {
+        addItem: function  (item) {
+            item.bigImageUrl = this.getBigImage(item);
+            items.push(item);
+        },
+        removeItem: function (item) { items.splice(items.indexOf(item), 1); },
+        toggleItem: function (item) {
+            ( this.hasItem(item) ) ? this.removeItem(item) : this.addItem(item);
+        },
+        getItems: function () { return items; },
+        hasItem: function (item) {
+            return ( _.find(items, function (i) { return i.id === item.id; }) ) ? true : false;
+        },
+        getBigImage: function (item) {
+            http.jsonp('http://api.dribbble.com/shots/' + item.id + '?callback=JSON_CALLBACK')
+            .success(function (response) {
+                item.bigImageUrl = response.image_url;
+            });
+        },
+        save: function () {
+            console.log(angular.toJson(items));
+            
+            // if no items yet just return
+            if (items.length === 0) return false;
+            
+            var json = angular.toJson(items);
+            
+            Mongodb.save({"x" : json}, function (response) {
+                console.log(response._id.$oid);
+            });
+        }
+    };
+    
+    return mdbrddb;
+}]);
+
+
+
+app.run( function($rootScope, mdbrddb) {
     $rootScope.full = false;
+    
 });
+
+function toolsCtrl ($scope, mdbrddb) {
+    $scope.saveMdbrd = function ($event) {
+        $event.preventDefault();
+        mdbrddb.save();
+    };
+}
 
 function searchCtrl ($scope, $resource, mdbrddb) {
 
@@ -55,8 +90,7 @@ function searchCtrl ($scope, $resource, mdbrddb) {
                     'url' : 'http:' + num.div.noscript.img.src,
                     'alt' : num.div.noscript.img.alt,
                     'href': num.a[0].href,
-                    'id'  : num.a[0].href.substr(7).split("-")[0],
-                    'selected' : false
+                    'id'  : num.a[0].href.substr(7).split("-")[0]
                 };
                 return newNum;
             });
@@ -69,7 +103,6 @@ function searchCtrl ($scope, $resource, mdbrddb) {
 
     $scope.toggleSelected = function (shot, $event) {
         $event.preventDefault();
-        // shot.selected = !shot.selected;
         mdbrddb.toggleItem(shot);
     };
 
@@ -80,7 +113,7 @@ function searchCtrl ($scope, $resource, mdbrddb) {
     $scope.changePage = function (page, $event) {
         $event.preventDefault();
         $scope.page = page;
-    }
+    };
 }
 
 function mdbrdCtrl ($scope, mdbrddb, $rootScope) {
